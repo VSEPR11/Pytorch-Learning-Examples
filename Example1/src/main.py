393fc0c8108ed1873bcd55d0eb613fd0adc78185
lyk <13627763786@163.com>
2023-12-11 16:11:20 +0800

    Example1

--- Example1/src/main.py
+++ Example1/src/main.py
@@ -56,7 +56,7 @@ test_loader = data.DataLoader(Dataset('../dataset/test/data/', '../dataset/test/
                               shuffle=True)
 # 定义损失函数，本次使用最简单的均方差损失函数
 criterion = nn.MSELoss()
-# 定义优化其，本次使用随机梯度下降算法
+# 定义优化器，本次使用随机梯度下降算法
 optimizer = optim.SGD(model.parameters(), lr=learn_rate)
 
 for epoch in range(epoch_num):  # 训练epoch_num次
@@ -76,16 +76,15 @@ print('\nTrain End')
 model.eval()
 
 with torch.no_grad():  # 禁用梯度
-
     # 计算误差，通过预期和实际值的差值的累加的均值来衡量
     det = 0
     for data in test_loader:
         x, y = data
         output = model(x)
-        for i in range(len(y)):
-            det += abs(y[i] - output[i]).item()
-        det /= len(y)
-    print(f'准确率: {(1 - det / len(train_loader)) * 100}%')
+        det += abs((output - y) / y).sum()
+        det /= y.numel()
+    det /= len(test_loader)
+    print(f'精度:{(1 - det) * 100}%')
 
     # 输出模型参数，由于线性层基本上就是 y = wT * x + b， 所以可以将第一个tensor视为拟合结果，可以看出，拟合是很好的
     for param in model.parameters():
